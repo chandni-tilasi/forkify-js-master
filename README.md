1829b1a4a990389964432e5b95018602784bf052
chandni-tilasi <96279259+chandni-tilasi@users.noreply.github.com>
2023-04-06 17:22:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# forkify Project
+
+Created By Abhideep Ghosh
+
+## About The Project
+
+1. Recipe application with custom recipe uploads.
+2. Follows Model View Controller Structure (MVC).
---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '39e1e104-8e03-4ffc-9fa7-e17545b47618';
+export const MODAL_CLOSE_SEC = 2.5;
